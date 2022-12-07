7191d4896ecd22e8a0555ddc6d30e85f9d4c7ad9
m1z1na <51057800+m1z1na@users.noreply.github.com>
2022-12-07 21:46:34 +0300

    Initial commit 07/12/22 (3)

---
+++ src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -0,0 +1,40 @@
+package ru.job4j.iterator;
+
+
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class EvenNumbersIterator implements Iterator<Integer> {
+
+    private int[] data;
+    private int index;
+
+    public EvenNumbersIterator(int[] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+//            int i = index;
+        while ( index < data.length ) {
+            if (data[index]%2==0){
+
+                break;
+            }
+            index++;
+        }
+
+        return index < data.length && data[index]%2==0;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        int upindex = index++;
+        return data[upindex];
+
+    }
+}
