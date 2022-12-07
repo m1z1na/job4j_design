633332a6dd3ba225187afa441fadafdc2a817eff
m1z1na <51057800+m1z1na@users.noreply.github.com>
2022-12-07 21:30:39 +0300

    Initial commit 07/12/22 (2)

---
+++ src/test/java/ru/job4j/assertj/BoxTest.java
@@ -0,0 +1,60 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BoxTest {
+    @Test
+    void isThisSphere() {
+        Box box = new Box(0, 10);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Sphere");
+    }
+
+    @Test
+    void isThisCube() {
+        Box box = new Box(8, 5);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Cube");
+    }
+
+
+    @Test
+    void checkNumberOfVertices() {
+        Box box = new Box(8, 5);
+        int line = box.getNumberOfVertices();
+        assertThat(line).isEqualTo(8);
+    }
+
+    @Test
+    void checkIsExist() {
+        Box box = new Box(8, 5);
+        assertThat(box.isExist()).isEqualTo(true);
+    }
+
+    @Test
+    void checkIsNotExist() {
+        Box box = new Box(9, 9);
+        assertThat(box.isExist()).isEqualTo(false);
+    }
+
+
+    @Test
+    void checkSphereArea() {
+        Box box = new Box(0, 10);
+        int name = (int) box.getArea();
+        assertThat(name).isEqualTo(1256);
+
+    }
+
+    @Test
+    void checkCubeArea() {
+        Box box = new Box(8, 5);
+        int name = (int) box.getArea();
+        assertThat(name).isEqualTo(150);
+
+    }
+
+
+}
