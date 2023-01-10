4609d9ed7feb4ca456816247aad66377052295e0
m1z1na <51057800+m1z1na@users.noreply.github.com>
2023-01-10 22:09:28 +0300

    #373618

---
+++ devicesScript.sql
@@ -0,0 +1,49 @@
+create table devices(
+    id serial primary key,
+    name varchar(255),
+    price float
+);
+
+create table people(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table devices_people(
+    id serial primary key,
+    device_id int references devices(id),
+    people_id int references people(id)
+);
+
+insert into devices(name) values ('Галя');
+insert into devices(name) values ('Валя');
+insert into devices(name) values ('Ваня');
+
+insert into devices(name, price) values ('Телефон', 5000 );
+insert into devices(name, price) values ('Наушники', 500 );
+insert into devices(name, price) values ('Холодильникн', 70000);
+insert into devices(name, price) values ('Утюг', 2500.00);
+
+
+
+insert into devices_people(device_id, people_id) values (1, 1);
+insert into devices_people(device_id, people_id) values (2, 1);
+insert into devices_people(device_id, people_id) values (3, 2);
+insert into devices_people(device_id, people_id) values (2, 3);
+
+Select s.name, avg(pp.price)
+from devices_people as ss
+join people s
+on ss.people_id = s.id
+join devices pp
+on pp.id = ss.device_id
+group by s.name;
+
+Select s.name, avg(pp.price) as sum
+from devices_people as ss
+join people s
+on ss.people_id = s.id
+join devices pp
+on pp.id = ss.device_id
+group by s.name
+having  avg(pp.price) > 5000;
