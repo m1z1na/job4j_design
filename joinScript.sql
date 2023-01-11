8f3b87cce575d5fcc0d93d1209b017e648849c75
m1z1na <51057800+m1z1na@users.noreply.github.com>
2023-01-11 20:01:11 +0300

    #373617 #379740

--- joinScript.sql
+++ joinScript.sql
@@ -1,6 +1,6 @@
 create table author(
     id serial primary key,
-    name varchar(255),
+    name varchar(255)
 );
 
 
@@ -10,12 +10,20 @@ create table book(
     author_id int references author(id)
 );
 
+
+insert into author(name) values ('Пушкин А.С.');
+insert into author(name) values ('Толстой Л.Н.');
+insert into author(name) values ('Семенов В.Г.');
+
+insert into people(name, passport_id) values ('Капитанская дочка', 1);
+insert into people(name, passport_id) values ('Война и Мир', 2);
+
 select bk.name as "Название книги", auth.name as "автор"
-    from author auth at join book bk on bk.author_id = auth.id;
+    from author auth join book bk on bk.author_id = auth.id;
 
 --авторы которые хоть что-то написали
 select distinct auth.name as "ФИО"
-    from author auth at join book bk on bk.author_id = auth.id;
+    from author auth join book bk on bk.author_id = auth.id;
 
 select distinct auth.name, bk.name as book_name
-    from author auth at join book bk on bk.author_id = auth.id;
+    from author auth join book bk on bk.author_id = auth.id;
