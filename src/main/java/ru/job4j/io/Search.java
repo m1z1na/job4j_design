9a9c2002432ca6eb43c20017a614238efb59512e
m1z1na <51057800+m1z1na@users.noreply.github.com>
2023-06-29 13:53:39 +0300

    10. Сканирование файловой системы [#106929  [#106929]]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -17,7 +17,6 @@ public class Search {
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
-
         return searcher.getPaths();
     }
 }
